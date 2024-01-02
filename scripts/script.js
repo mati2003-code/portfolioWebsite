434ab682f6eaa78dee5da9231f41f249fc2d5cd3
mati2003-code <mateusz.dziobakop.pl@op.pl>
2024-01-02 10:50:44 +0100

    add header styles & nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,11 @@
 'use strict';
 
-const siteMenu = document.querySelector(".site-menu");
-const hamburgerMenuButton = document.querySelector(".fa-solid");
+const hamburgerButton = document.querySelector('.hamburger-menu');
+const siteMenu = document.querySelector('.site-menu');
 
-const menuSettings = () => {
-  hamburgerMenuButton.addEventListener("click", () => {
-    siteMenu.classList.add("animation-appear");
-    siteMenu.classList.toggle("display-block");
-  });
-
-  siteMenu.addEventListener("click", () => {
-    siteMenu.classList.remove("display-block");
-  });
-
-  window.addEventListener("resize", () => {
-    if (window.innerWidth >= 768) {
-      siteMenu.classList.add("animation-disappear");
-
-      setTimeout(() => {
-        siteMenu.classList.remove("display-block");
-      }, 500);     
-    } else {
-      siteMenu.classList.remove("animation-disappear");
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", menuSettings);
+hamburgerButton.addEventListener('click', () => {
+  siteMenu.classList.toggle('transform');
+}) 
 
 
 
